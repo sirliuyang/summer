8aabd36153686ab1d3d9ae09c76189526478172c
Leon <88978827@qq.com>
2021-02-15 14:22:29 +0800

    add CglibProxy

---
+++ src/main/java/com/formula/summer/patterns/proxy/CglibProxy.java
@@ -0,0 +1,24 @@
+package com.formula.summer.patterns.proxy;
+
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CglibProxy implements MethodInterceptor {
+
+    public Object getInstance(final Object target) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(target.getClass());
+        enhancer.setCallback(this);
+        return enhancer.create();
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("Cglib Proxy");
+        return methodProxy.invoke(o, objects);
+    }
+}
